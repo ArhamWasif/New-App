ce8940ffc2d8b19a517b27eec4acd09698317b3e
Arham Wasif <163019304+ArhamWasif@users.noreply.github.com>
2025-05-16 06:23:46 +0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+// import {View, Text} from 'react-native';
+// import React from 'react';
+// import BottomSheet from './src/BottomSheet';
+// import 'react-native-gesture-handler';
+// import 'react-native-reanimated';
+// import {GestureHandlerRootView} from 'react-native-gesture-handler';
+// const App = () => {
+//   return (
+//     <GestureHandlerRootView style={{flex: 1}}>
+//       <View>
+//         <BottomSheet />
+//       </View>
+//     </GestureHandlerRootView>
+//   );
+// };
+
+// export default App;
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
